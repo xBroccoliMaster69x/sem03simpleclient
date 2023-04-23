14c031868d8628903a53459f7e415c99fc9f8d16
xBroccolMaster69x <alexandega@uia.no>
2023-04-23 14:44:56 +0000

    kryptering

---
+++ go.mod
@@ -0,0 +1,7 @@
+module tcp_client.go
+
+go 1.19
+
+require (
+	github.com/xBroccoliMaster69x/is105sem03 v1.0.0
+)
--- tcp_client.go
+++ tcp_client.go
@@ -4,15 +4,22 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/xBroccoliMaster69x/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.4:8000")
+	conn, err := net.Dial("tcp", "172.17.0.2:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
+
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("kryptert melding: ", string(kryptertMelding))
+	_,err = conn.Write([]byte(string(kryptertMelding)))
+
+
 
  	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
@@ -25,4 +32,5 @@ func main() {
 	} 
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+
 }
